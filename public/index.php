cea37e6cee0cb0002d795201b60527a0d4da2ad9
Ibrahima D <ibrahima.diallo@display-interactive.com>
2021-01-30 00:04:20 +0100

    event dispatcher

--- public/index.php
+++ public/index.php
@@ -3,14 +3,15 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
+use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
 use Symfony\Component\HttpKernel\Controller\ControllerResolver;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\RequestContext;
 
 require_once __DIR__.'/../vendor/autoload.php';
+$dispatcher = new EventDispatcher();
 
 $request = Request::createFromGlobals();
 
@@ -21,7 +22,26 @@ $urlMatcher = new UrlMatcher($routes, new RequestContext());
 $controllerResolver = new ControllerResolver();
 $argumentResolver = new ArgumentResolver();
 
-$framework = new Framework\Simplex($urlMatcher, new ControllerResolver(), new ArgumentResolver());
+$dispatcher = new EventDispatcher();
+$dispatcher->addListener('kernel.request', function (){
+    dump("hollla request");
+});
+
+$dispatcher->addListener('kernel.controller', function (){
+    dump("hollla controller");
+});
+
+$dispatcher->addListener('kernel.arguments', function (){
+    dump("hollla arguments");
+});
+
+
+
+$dispatcher->addListener('kernel.response', function (){
+    dump("hollla response");
+});
+$framework = new Framework\Simplex($dispatcher, $urlMatcher, new ControllerResolver(), new ArgumentResolver());
 $response = $framework->handle($request);
+
 
 $response->send();
--- src/Framework/Simplex.php
+++ src/Framework/Simplex.php
@@ -2,30 +2,38 @@
 
 namespace Framework;
 
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Controller\ArgumentResolverInterface;
 use Symfony\Component\HttpKernel\Controller\ControllerResolverInterface;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
+use Symfony\Contracts\EventDispatcher\Event;
+
+;
 
 class Simplex
 {
-    private UrlMatcherInterface $matcher;
-    private ControllerResolverInterface $controllerResolver;
-    private ArgumentResolverInterface $argumentResolver;
+    protected EventDispatcherInterface $dispatcher;
+    protected UrlMatcherInterface $matcher;
+    protected ControllerResolverInterface $controllerResolver;
+    protected ArgumentResolverInterface $argumentResolver;
     
     /**
      * Simplex constructor.
+     * @param EventDispatcherInterface $dispatcher
      * @param UrlMatcherInterface $matcher
      * @param ControllerResolverInterface $controllerResolver
      * @param ArgumentResolverInterface $argumentResolver
      */
     public function __construct(
+        EventDispatcherInterface $dispatcher,
         UrlMatcherInterface $matcher,
         ControllerResolverInterface $controllerResolver,
         ArgumentResolverInterface $argumentResolver
     ) {
+        $this->dispatcher = $dispatcher;
         $this->matcher = $matcher;
         $this->controllerResolver = $controllerResolver;
         $this->argumentResolver = $argumentResolver;
@@ -36,13 +44,18 @@ class Simplex
     {
         $this->matcher->getContext()->fromRequest($request);
         
+        $this->dispatcher->dispatch(new Event(), 'kernel.request');
+        
         try {
-            
             $request->attributes->add($this->matcher->match($request->getPathInfo()));
+            $this->dispatcher->dispatch(new Event(), 'kernel.controller');
             $controller = $this->controllerResolver->getController($request);
+            $this->dispatcher->dispatch(new Event(), 'kernel.arguments');
             $arguments = $this->argumentResolver->getArguments($request, $controller);
             
-            return call_user_func_array($controller, $arguments);
+            $response = call_user_func_array($controller, $arguments);
+            $this->dispatcher->dispatch(new Event(), 'kernel.response');
+            return $response;
         } catch (ResourceNotFoundException $exception) {
             return new Response('La page nexiste pas', 404);
         } catch (\Exception $exception) {
